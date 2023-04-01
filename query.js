dfed42b96b96e4478c9d91a553c0e760a00d5626
mianraheel85 <124381337+mianraheel85@users.noreply.github.com>
2023-03-31 22:32:50 -0400

    Add files via upload

---
+++ query.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    console.log("Let's get ready to party with JQuery!");
+
+
+   $("article img").addClass("image-center");
+
+  $("article p:last-child").remove();
+
+  $("#title").css("font-size", Math.random() * 100);
+  $("ol").append($("<li>", {text: "The puppy looks naughty"}));
+
+   $("aside")
+   .empty()
+   .append($("<p>", {text: "Sorry about that list :( "}));
+   
+   $(".form-control").on('keyup blur change', function() {
+    let red= $(".form-control").eq(0).val();
+    let blue= $(".form-control").eq(1).val();
+    let green= $(".form-control").eq(2).val();
+$("body").css("background-color",
+"rgb(" +red + ", " + green + ", " + blue +")");
+
+   });
+$("img").on('click', function(e) {
+    $(e.target).remove();
+});
+
+
+});
+
